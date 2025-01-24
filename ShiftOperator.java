47db26f92e5258d7dab1c1e66c585310d9c8d43a
Akshaya Reddy Anna Reddy <arudraar2023@gmail.com>
2025-01-24 23:38:27 +0530

    Add files via upload

---
+++ ShiftOperator.java
@@ -0,0 +1,17 @@
+public class ShiftOperator {
+    public static void main(String[] args) {
+        int a = 16;
+        int b = 4;
+        
+        System.out.println("Left Shift Operator (<<):");
+        System.out.println("a << 1: " + (a << 1));
+        System.out.println("b << 2: " + (b << 2));
+
+        System.out.println("\nRight Shift Operator (>>):");
+        System.out.println("a >> 1: " + (a >> 1));
+        System.out.println("b >> 2: " + (b >> 2));
+    }
+}
+
+
+
